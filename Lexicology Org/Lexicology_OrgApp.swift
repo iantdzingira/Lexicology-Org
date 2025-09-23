fc2fad4350c4165932a165bb04577e5ad3acd89d
Ian Dzingira T <ianrikes06@gmail.com>
2025-09-23 14:00:43 +0200

    Initial Commit

---
+++ Lexicology Org/Lexicology_OrgApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lexicology_OrgApp.swift
+//  Lexicology Org
+//
+//  Created by Ian. T. Dzingira on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lexicology_OrgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
